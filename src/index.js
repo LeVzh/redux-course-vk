bbd6431a1ac01e86ede244b092a7fc590209d46c
LeVzh <ion_king_zh9@mail.ru>
2018-11-23 14:24:13 +0300

    add actions and Year-btns, using dispatch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {store} from './store/configureStore';
-import App from './App';
+import App from './containers/App';
+
+import './index.css'
 
 import * as serviceWorker from './serviceWorker';
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 const initialState = {
-    name: 'Anonim',
+    name: 'Aноним',
 }
 
 export function userReducer(state = initialState) {
